d576a1e2f2554482af7c52f0bc107ac0050457f5
Daolaa <403366963@qq.com>
2019-08-01 15:12:56 +0800

    gao:代码自动生成

--- pinyougou-manager-web/src/main/java/com/pinyougou/manager/controller/BrandController.java
+++ pinyougou-manager-web/src/main/java/com/pinyougou/manager/controller/BrandController.java
@@ -3,6 +3,10 @@ package com.pinyougou.manager.controller;
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.pinyougou.pojo.TbBrand;
 import com.pinyougou.sellergoods.service.BrandService;
+import entity.PageResult;
+import entity.Result;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,7 +23,8 @@ import java.util.List;
 @RequestMapping("/brand")
 public class BrandController {
 
-    @Reference
+    @Autowired
+
     private BrandService brandService;
 
     /**
@@ -30,5 +35,86 @@ public class BrandController {
     @RequestMapping("/findAll")
     public List<TbBrand> findAll() {
         return brandService.findAll();
+    }
+
+    /**
+     * 返回分页列表
+     * @param page
+     * @param rows
+     * @return
+     */
+    @RequestMapping("/findPage")
+    public PageResult findPage(int page , int rows){
+
+        return brandService.findPage(page,rows);
+    }
+
+    /**
+     * 添加品牌
+     * @return
+     */
+    @RequestMapping("/add")
+    public Result add(@RequestBody TbBrand brand){
+        try {
+            brandService.add(brand);
+            return new Result(true,"添加成功");
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new Result(false,"添加失败");
+        }
+    }
+
+    /**
+     * 根据ID 查询实体
+     * @param id
+     * @return
+     */
+    @RequestMapping("/findOne")
+    public TbBrand findOne(Long id){
+        return brandService.findOne(id);
+    }
+
+    /**
+     * 修改
+     * @param brand
+     * @return
+     */
+    @RequestMapping("/update")
+    public Result update(@RequestBody TbBrand brand){
+        try {
+            brandService.update(brand);
+            return new Result(true,"修改成功");
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new Result(false,"修改失败");
+        }
+    }
+
+    /**
+     * 根据ID 批量删除
+     * @param ids
+     * @return
+     */
+    @RequestMapping("/delete")
+    public Result delete(Long[] ids){
+        try {
+            brandService.delete(ids);
+            return new Result(true,"删除成功");
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new Result(false,"删除失败");
+        }
+    }
+
+    /**
+     * 搜索方法
+     * @param brand
+     * @param page
+     * @param rows
+     * @return
+     */
+    @RequestMapping("/search")
+    public PageResult search(@RequestBody TbBrand brand,int page,int rows){
+        return brandService.search(brand,page,rows);
     }
 }
--- pinyougou-sellergoods-interface/src/main/java/com/pinyougou/sellergoods/service/BrandService.java
+++ pinyougou-sellergoods-interface/src/main/java/com/pinyougou/sellergoods/service/BrandService.java
@@ -1,6 +1,7 @@
 package com.pinyougou.sellergoods.service;
 
 import com.pinyougou.pojo.TbBrand;
+import entity.PageResult;
 
 import java.util.List;
 
@@ -13,5 +14,45 @@ public interface BrandService {
      * 返回全部列表
      * @return
      */
-    public List<TbBrand> findAll();
+    List<TbBrand> findAll();
+
+    /**
+     * 返回分页结果
+     * @return
+     */
+    PageResult findPage(int pageNum,int pageSize);
+
+    /**
+     * 品牌增加
+     * @param brand
+     */
+    void add(TbBrand brand);
+
+    /**
+     * 根据ID 获取实体
+     * @param id
+     * @return
+     */
+    TbBrand findOne(Long id);
+
+    /**
+     * 修改实体
+     * @param brand
+     */
+    void update(TbBrand brand);
+
+    /**
+     * 根据ID 删除
+     * @param ids
+     */
+    void delete(Long[] ids);
+
+    /**
+     * 条件查询
+     * @param tbBrand
+     * @param pageNum
+     * @param pageSize
+     * @return
+     */
+    PageResult search(TbBrand tbBrand,int pageNum,int pageSize);
 }
